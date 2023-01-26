14a44b8cfebb7d70632a3bcc6fcc7cf43da72f93
Wade <wade@pop-os.localdomain>
2023-01-26 18:33:10 -0500

    updated some information to Automation_practice.py

--- Python_requests_Scriting/Automation_practice.py
+++ Python_requests_Scriting/Automation_practice.py
@@ -1,11 +1,11 @@
+# information came from this video: https://youtu.be/tb8gHvYlCFs
 # This is practice for scripting automation
 # A good resource for this is httpbin.org
-
 
 import requests
 
 # input URL inside of the get function
-r = requests.get('')
+#r = requests.get('')
 
 # This will display a response code to the terminal
 #print(r)
@@ -29,4 +29,32 @@ r = requests.get('')
 # How to get image header information
 #print(r.headers)
 
+# This is for a GET request
+#payload = {'page': 2, 'count': 25}
+#r = requests.get('https://httpbin.org/get', params=payload)
 
+# This if for a POST request
+#payload = {'username': '', 'password': ''}
+#r = requests.post('https://httpbin.org/post', data=payload)
+
+#print(r.headers)
+#print(r.text)
+#print(r.url)
+
+# This makes a dictionary of the json outputed data
+#print(r.json())
+
+#r_dict = r.json()
+#print(r_dict['form'])
+
+# Trying out the /basic-auth request from httpbin.org
+#r = requests.get('https://httpbin.org/basic-auth/username/password', auth=('username', 'password')) # the data inside the auth tuple must match the data in the url
+#print(r.text)
+
+# if you use the wrong credentials for the basic-auth request above you will get a Response [401], this can be seen with the following print
+#print(r)
+
+# testing timeout
+#r = requests.get('https://httpbin.org/delay/6', timeout=3)
+
+#print(r)
